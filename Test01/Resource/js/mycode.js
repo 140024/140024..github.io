f907282c6ab7abb77672c4a9c709a7ca7cecf095
140024 <101464363+140024@users.noreply.github.com>
2022-03-29 20:36:05 +0700

    Add files via upload

---
+++ Test01/Resource/js/mycode.js
@@ -0,0 +1,9 @@
+var img1 = "images\Miquang02.jpg";
+var img2 = "images\spingroll02.jpg";
+
+function ChangeImg(){
+}
+
+// function UnDo(){
+//     document.getElementById('image').style.backgroundImage = "";
+// }
